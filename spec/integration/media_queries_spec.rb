e74ff1bbfc3e99dd2843aca4fec7359a40756a52
jede <johannes.edelstam@gmail.com>
2012-03-01 22:00:12 +0100

    Custom column widths

--- spec/integration/media_queries_spec.rb
+++ spec/integration/media_queries_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 require 'sass'
 require 'longitude'
 
-describe 'Media queries' do
-  describe 'standard columns' do
-    before :each do
-      template = """
-      @import 'longitude';
+def render(template)
+  template = "@import 'longitude'; " + template
+  Sass::Engine.new(template, syntax: :scss).render
+end
 
+describe 'Media queries' do
+  describe 'standard column widths' do
+    before :each do
+      @css = render """
       @include longitude(2 4) {
         p { color: blue; }
       }
       """
-      sass_engine = Sass::Engine.new(template, syntax: :scss)
-      @css = sass_engine.render
     end
 
     it 'includes lower bound' do
@@ -26,6 +27,23 @@ describe 'Media queries' do
 
     it 'includes the higher bound' do
       @css.should include('@media (min-width: 321px)')
+    end
+  end
+
+  describe 'custom column widths' do
+    before :each do
+      @css = render """
+      $longitude-gutter-width: 5px;
+      $longitude-column-width: 10px;
+
+      @include longitude(2 4) {
+        p { color: blue; }
+      }
+      """
+    end
+
+    it 'renders the middle stop according to the custom widths' do
+      @css.should include('@media (min-width: 41px) and (max-width: 80px)')
     end
   end
 end
