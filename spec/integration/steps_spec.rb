a896168fe01191661892c2fa325b84467b8b4ca2
Johannes Edelstam <johannes@edelst.am>
2012-03-30 16:05:33 +0200

    Use the last step value as default

--- spec/integration/steps_spec.rb
+++ spec/integration/steps_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Steps' do
   before :each do
     @css = render """
-      @include longitude(2 4) {
+      @include longitude(2 4 6) {
         p { width: steps(10px 20px); }
       }
     """
@@ -11,9 +11,10 @@ describe 'Steps' do
 
   it 'sets width according to the given steps' do
     @css.should include("width: 10px;")
+    @css.should include("width: 20px;")
   end
 
-  it 'sets width according to the given steps' do
-    @css.should include("width: 20px;")
+  it 'sets the last value as the default if it is missing' do
+    @css.should match(/@media \(min-width: 481px\).* width: 20px;/m)
   end
 end
